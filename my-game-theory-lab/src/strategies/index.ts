7b6b1f6d7118b8a8b028e4ac4346eb230bf46539
Umut Dinçer Yananer <umutdncr@gmail.com>
2025-10-02 02:55:24 +0300

    feat: add grudger, prober, alternator, and pavlov variants

--- my-game-theory-lab/src/strategies/index.ts
+++ my-game-theory-lab/src/strategies/index.ts
@@ -1,19 +1,28 @@
-// Import all strategies
 export { alwaysCooperate } from './alwaysCooperate';
 export { alwaysDefect } from './alwaysDefect';
 export { titForTat } from './titForTat';
 export { random } from './random';
+export { grudger } from './grudger';
+export { prober } from './prober';
+export { alternate } from './alternate';
+export { pavlov } from './pavlov';
 
-// Import for convenience
 import { alwaysCooperate } from './alwaysCooperate';
 import { alwaysDefect } from './alwaysDefect';
 import { titForTat } from './titForTat';
 import { random } from './random';
+import { grudger } from './grudger';
+import { prober } from './prober';
+import { alternate } from './alternate';
+import { pavlov } from './pavlov';
 
-// Array of all default strategies
 export const defaultStrategies = [
   alwaysCooperate,
   alwaysDefect,
   titForTat,
-  random
+  random,
+  grudger,
+  prober,
+  alternate,
+  pavlov,
 ];
